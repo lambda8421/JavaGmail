fb84f4357fc4cb16ab541dcc0df711b5e51764d6
Yongzhao Zhang <lambda8421@163.com>
2018-07-28 19:29:53 -0400

    Initial commit

---
+++ src/main/java/Email.java
@@ -0,0 +1,6 @@
+public class Email {
+    public static final String emailAddress = "your email address";
+    public static final String emailPassword = "your email password";
+    public static final String toEmailAddress = "email address can receive email";
+
+}
---
+++ src/main/java/SendMailTLS.java
@@ -0,0 +1,44 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendMailTLS {
+
+    public static void main(String[] args) {
+
+        final String username = Email.emailAddress;
+        final String password = Email.emailPassword;
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Email.emailAddress));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(Email.toEmailAddress));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
